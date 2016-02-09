f8f977b3647e28a93450ce7f2da5fa8e3aa03344
Enrique Zepeda <ingzepe@yahoo.com.mx>
2016-02-09 13:27:26 -0600

    Agregando librerias
    Zend, Angular, Angular Material

---
+++ module/Auth/config/module.config.php
@@ -0,0 +1,70 @@
+<?php
+namespace Auth;
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'auth' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route'    => '/auth',
+                    'defaults' => array(
+                        'controller' => 'Auth',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            'auth' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/auth',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Auth\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Auth\Controller\Index' => Controller\IndexController::class
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'auth/index/index' => __DIR__ . '/../view/auth/index/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
+);
